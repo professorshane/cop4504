3961479e207f0b9f17018dc4a14fc8e9d0f0e777
Shane <hamiltonshane@hotmail.com>
2014-10-12 16:11:23 -0400

    Shane Hamilton

---
+++ COP4504GIT/src/cop4504git/COP4504GIT.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cop4504git;
+
+/**
+ *
+ * @author Shane Hamilton
+ */
+public class COP4504GIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Shane Hamilton");
+        //System.out.println("John Doe");
+        //Enter your System.out.printlin("your name here"); below and submit your
+        //changes for full credit.
+        
+    }
+    
+}
