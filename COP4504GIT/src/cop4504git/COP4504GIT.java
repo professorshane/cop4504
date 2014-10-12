76cbdbbbc80bda2079c024229d69023e1d54d2d3
Shane <hamiltonshane@hotmail.com>
2014-10-12 17:20:38 -0400

    Shane Hamilton

--- COP4504GIT/src/cop4504git/COP4504GIT.java
+++ COP4504GIT/src/cop4504git/COP4504GIT.java
@@ -18,7 +18,7 @@ public class COP4504GIT {
         System.out.println("Shane Hamilton");
         //System.out.println("John Doe");
         //Enter your System.out.printlin("your name here"); below and submit your
-        //changes for full credit.
+        //changes for full credit!
         
     }
     
