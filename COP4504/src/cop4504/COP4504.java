dcfd0cda784d44f5ae069ffaece5970b67a2cc3f
Shane <hamiltonshane@hotmail.com>
2014-10-13 11:53:55 -0400

    Shane Hamilton

---
+++ COP4504/src/cop4504/COP4504.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cop4504;
+
+/**
+ *
+ * @author Shane Hamilton
+ */
+public class COP4504 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Test
+    }
+    
+}
