f841d8176d793c198040d707f71d84caf059fe9e
Shane <hamiltonshane@hotmail.com>
2014-10-13 11:54:51 -0400

    Shane Hamilton

--- COP4504/src/cop4504/COP4504.java
+++ COP4504/src/cop4504/COP4504.java
@@ -15,7 +15,7 @@ public class COP4504 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Test
+        System.out.println("Shane Hamilton");
     }
     
 }
